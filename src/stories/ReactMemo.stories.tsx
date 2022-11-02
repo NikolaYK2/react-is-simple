88cdf7dfd62c59a503ba23727297b3499af10774
k2 <izmail.k2@gmail.com>
2022-11-02 14:27:18 +0300

    useMemo

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -1,5 +1,5 @@
 import {ComponentStory} from "@storybook/react";
-import React, {useState} from "react";
+import React, {useMemo, useState} from "react";
 
 export default {
     title: 'components/React.memo demo'
@@ -11,13 +11,15 @@ const NewMessagesCounter = (props: { count: number }) => {
 
 // type UsrType ={users: Array<string>}
 const Users = (props: { users: Array<string> }) => {
+    console.log('user')
     return <div>
         {props.users.map((u, i) => <div key={i}>{u}</div>)}
     </div>
 }
 const UsersShell = React.memo(Users);
+//=========================================================================
 
-export const Example: ComponentStory<any> = () => {
+const Template: ComponentStory<any> = () => {
     const [counter, setCounter] = useState(0)
     const [users, setUsers] = useState(['Nik', 'Vita', 'Dima', 'Vova', 'Denis'])
 
@@ -37,5 +39,84 @@ export const Example: ComponentStory<any> = () => {
         <UsersShell users={users}/>
     </>
 }
+export const Example = Template.bind({})
+//====================================================
 
-// const Template = Example.bind({})
+const Template1: ComponentStory<any> = () => {
+    const [a, setA] = useState(1);
+    const [b, setB] = useState(5);
+
+    let resultA = 1;
+    let resultB = 1;
+
+    //Внутри функция будет делать сложные вычисления, вторым параметром[] передаем сложные зависимости
+    //на которыу реакт обращает внимание и решает перезапускать useMemo или нет
+    //если оставить пустое[] - значит что запомниться один раз и будет возвращать
+    //одно и то-же значение
+    resultA = useMemo(() => {
+        let tempResultA = 1;
+        for (let i = 1; i <= a; i++) {
+            let fake = 0;
+            while (fake < 100000000) {
+                fake++
+                const fakeValue = Math.random();
+            }
+            tempResultA = tempResultA * i;
+        }
+        return tempResultA;
+    }, [a]);
+
+
+    for (let i = 1; i <= b; i++) {
+        resultB = resultB * i;
+    }
+    return (
+        <>
+            <input value={a} onChange={(e) => setA(+e.currentTarget.value)}/>
+            <input value={b} onChange={(e) => setB(+e.currentTarget.value)}/>
+            <hr/>
+            <div>
+                Result for a: {resultA}
+            </div>
+            <div>
+                Result for b: {resultB}
+            </div>
+        </>
+    )
+}
+//сложное вычисление
+export const DifficultCourtingExample = Template1.bind({})
+//====================================================================
+
+const Template2: ComponentStory<any> = () => {
+    console.log('counter')
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(['Nik', 'Vita', 'Dima', 'Vova', 'Denis'])
+
+    const onClickHandler = () => {
+        setCounter(counter + 1)
+    }
+
+    const newUsers = useMemo(()=>{
+       return  users.filter(u=>u.toLowerCase().indexOf('a')> -1);
+    },[users]);
+
+    const addUserHandler = () => {
+        setUsers([...users, 'Nasta'])
+        // users.push('Nasta' + new Date().getTime())
+        // setUsers(users);
+    }
+
+
+    return <>
+        <button onClick={onClickHandler}>+</button>
+        <button onClick={addUserHandler}>add user</button>
+        {counter}
+        <UsersShell users={newUsers}/>
+    </>
+}
+
+
+//сложное вычисление
+export const helpsToReactMemo = Template2.bind({})
+//================================================================
