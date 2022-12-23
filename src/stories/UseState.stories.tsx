41719705d61acbf095e75c2a42bef926ee2e8d74
k2 <izmail.k2@gmail.com>
2022-12-23 21:39:59 +0300

    навороченный useState refactor

--- src/stories/UseState.stories.tsx
+++ src/stories/UseState.stories.tsx
@@ -1,16 +1,33 @@
 import {ComponentStory} from "@storybook/react";
-import {useState} from "react";
+import {useMemo, useState} from "react";
 
 export default {
     title: 'components/UseStateStories demo'
 }
 
+function generateData() {
+    console.log('generateData');
+    return 1;
+}
+
 const Template: ComponentStory<any> = () => {
-    console.log('UseStateStories');
-    const [counter, setCounter] = useState(0)
+    console.log('UseState');
+
+    // const initValue = useMemo(generateData,[]);
+    //useMemo в данном случаи не нужен, useState сам себя оптимизирует и
+    // вызовет тяжелую функцию один раз
+
+    const [counter, setCounter] = useState(generateData);
+    //В setCounter передаем новое значние, а можно передовать сюда функцию
+
+    // const changer =(state:number)=> state + 1;
     return (
         <>
-            <button onClick={()=>setCounter(counter + 1)}></button>
+            <button onClick={() => setCounter(state => state + 1)}>+</button>
+            {/*<button onClick={()=>setCounter(changer)}>+</button>*/}
+            {/*<button onClick={()=>setCounter(counter + 1)}>+</button>*/}
+
+            {counter}
         </>
     );
 }
