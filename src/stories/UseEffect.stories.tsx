75ed7994a89307c482a076c41ff5311e30ad372a
k2 <izmail.k2@gmail.com>
2022-12-24 21:50:20 +0300

    useEffect \ setTimeout \ setInterval

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -6,7 +6,6 @@ export default {
 }
 
 
-
 const Template: ComponentStory<any> = () => {
 
 
@@ -14,27 +13,85 @@ const Template: ComponentStory<any> = () => {
     const [fake, setFake] = useState(1);
     console.log('UseState');
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log('каждый раз, потому что без зависимостей');
         document.title = String(counter);
     },)
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log('useEffect только один раз, когда компонента вмантировалась');
         document.title = String(counter);
-    },[])
+    }, [])
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log('useEffect запускается только если меняется зависимость');
         document.title = String(counter);
-    },[counter])
+    }, [counter])
 
     return (
         <>
             Hello, {counter} {fake}
-            <button onClick={()=>setFake(fake + 1)}>Add fake</button>
-            <button onClick={()=>setCounter(counter+1)}>Add count</button>
+            <button onClick={() => setFake(fake + 1)}>Add fake</button>
+            <button onClick={() => setCounter(counter + 1)}>Add count</button>
         </>
     );
 }
 export const UseEffect = Template.bind({});
+
+const Template1: ComponentStory<any> = () => {
+
+
+    const [counter, setCounter] = useState(1);
+    const [stopTime, setStopTime] = useState(setInterval(()=>{}));
+    console.log('UseState');
+
+    // useEffect(()=>{
+    //     console.log('setTimout');
+    //     setTimeout(()=>{
+    //         document.title = String(counter);
+    //     },1000)
+    // },[])
+    //     useEffect(() => {
+    //         console.log('tick');
+    //         setStopTime(setInterval(() => {
+    //             setCounter(state => state + 1);
+    //         }, 1000))
+    //
+    //     }, []);
+
+    return (
+        <>
+            Hello, counter:{counter}
+            <button onClick={()=>clearInterval(stopTime)}>stop</button>
+        </>
+    );
+}
+export const UseEffectSetTimout = Template1.bind({});
+
+const Template2: ComponentStory<any> = () => {
+
+    const [time, setTime] = useState(new Date());
+
+    // useEffect(()=>{
+    //     console.log('setTimout');
+    //     setInterval(()=>{
+    //             setTime(new Date())
+    //     },1000)
+    // },[])
+    // useEffect(()=>{
+    //     console.log('setTimout');
+    //     setTimeout(function time(){
+    //             setTime(new Date());
+    //             setTimeout(time,1000);
+    //     },1000);
+    //
+    // },[])
+
+        const min = time.toLocaleTimeString('ru-ru')
+    return (
+        <>
+            time: {min}
+        </>
+    );
+}
+export const clock = Template2.bind({});
